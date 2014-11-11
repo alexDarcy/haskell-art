afc0566d1de1140fea87d7bce8fe2391e8ddc5dc
Alexis Praga <praga@cerfacs.fr>
2014-11-11 02:08:58 +0100

    Finally, it's working.

--- hexVariation.hs
+++ hexVariation.hs
@@ -4,6 +4,7 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import System.Random
 import Numeric
+import qualified Debug.Trace as D
 
 -- Note : the side of a hexagon is the radius of its circumscribed circle
 
@@ -17,12 +18,15 @@ hexagon' :: Diagram B R2
 hexagon' = mconcat [arc1 # translateX (-1)
                   , vrule (2*h)
                   , arc1 # rotateBy (1/2) # translateX 1 
-                  , polygon (with & polyType .~ PolyRegular 6 1)
+--                  , hexagon 1
                   ]
     where
       arc1 = arc' 0.5 (-pi/3 @@ rad) (pi/3 @@ rad) 
 
-rotateHexagon' n = hexagon' # rotate (2*n*pi/3 @@ rad)
+rotateHexagon' :: Int -> Diagram B R2
+rotateHexagon' n = hexagon' # rotate (n'*2*pi/3 @@ rad)
+  where
+    n' = fromIntegral n
 
 centerPosition :: Int -> Int -> (Double, Double)
 centerPosition x y 
@@ -32,14 +36,16 @@ centerPosition x y
     x' = fromIntegral x
     y' = fromIntegral y
 
-hexVariation :: StdGen -> Diagram B R2
-hexVariation g = position (zip (map p2 pos) (map rotateHexagon' test))
---hexVariation g = position (zip (map p2 pos) (repeat hexa ))
+generateAngles :: StdGen -> [Int]
+generateAngles g = randomRs (0, 2) g
+
+hexVariation :: Int -> StdGen -> Diagram B R2
+hexVariation nb g = position (zip (map p2 pos) (map rotateHexagon' test))
   where 
-    pos = [(centerPosition x y) | x <- [0..10], y <- [0..2]]
-    test = take 20 $ randomRs (0, 2) g
+    pos = [(centerPosition x y) | x <- [0..nb], y <- [0..nb]]
+    test = take ((nb+1)*(nb+1)) $ generateAngles g
     hexa = hexagon'
 
 main = do
   g <- newStdGen
-  mainWith $ hexVariation g
+  mainWith $ hexVariation 30 g
