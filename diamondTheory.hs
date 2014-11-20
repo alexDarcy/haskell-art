8f9ad54454ec364f90fcfed405c5b3d702ff8a42
Alexis Praga <praga@cerfacs.fr>
2014-11-20 21:54:52 +0100

    Creating a tile with position, and from a list of angles and colors.

--- diamondTheory.hs
+++ diamondTheory.hs
@@ -3,6 +3,7 @@
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import System.Random
+import Data.List
 --import qualified Debug.Trace as D
 
 side = sqrt(2)
@@ -37,32 +38,47 @@ triangleRight color = triangleRect # fc color #lc color # lw none
 -- Color of the right triangle is the inverse of the left triangle
 -- We enforce old behaviour for the origin of the tile: we want the point of
 -- tangency, enforced by "align"
-tile :: Diagram B R2
-tile = beside (r2 (1,-1)) (triangleLeft black # align (r2 (1, -1)))
-                                (triangleRight white)
+tile :: Colour Double -> Diagram B R2
+tile color = beside (r2 (1,-1)) (triangleLeft color # align (r2 (1, -1)))
+                                (triangleRight color')
+  where color' = if color == white then black else white
 
-rotateTile :: (Diagram B R2, Int) -> Diagram B R2
---rotateTile x | D.trace ("rotatetile" ++ show (fromIntegral $ snd x)) False = undefined
-rotateTile x = fst x # rotate (x'*pi/2 @@ rad) 
-  where x' = fromIntegral $ snd x
+-- rotateTile :: (Diagram B R2, Int) -> Diagram B R2
+-- --rotateTile x | D.trace ("rotatetile" ++ show (fromIntegral $ snd x)) False = undefined
+-- rotateTile x = fst x # rotate (x'*pi/2 @@ rad) 
+--   where x' = fromIntegral $ snd x
+-- 
+-- lineTiles :: Int -> Diagram B R2
+-- lineTiles seed = cat (r2 (1, 0)) $ tiles'
+--   where 
+--     tiles' = map rotateTile $ zip (take 4 $ repeat tile) angles
+--     angles = take 4 $ randomRs (1, 4) (mkStdGen seed)
+-- 
+-- largeTile seed = vcat $ map lineTiles seeds
+--   where seeds = take 4 $ randoms (mkStdGen seed)
+-- 
+-- lineLargeTiles seed = hcat' (with & sep .~ 1) $ map largeTile seeds
+--   where seeds = take 4 $ randoms (mkStdGen seed)
+-- 
+-- setLines seed = vcat' (with & sep .~ 1) $ map lineLargeTiles seeds
+--   where seeds = take 4 $ randoms (mkStdGen seed)
 
-lineTiles :: Int -> Diagram B R2
-lineTiles seed = cat (r2 (1, 0)) $ tiles'
+tile' :: (Int, Int) -> Diagram B R2
+tile' x = tile color # rotate (a'*pi/2 @@ rad)
   where 
-    tiles' = map rotateTile $ zip (take 4 $ repeat tile) angles
-    angles = take 4 $ randomRs (1, 4) (mkStdGen seed)
+    color = generateColor $ fst x
+    a' = fromIntegral $ snd x
 
-largeTile seed = vcat $ map lineTiles seeds
-  where seeds = take 4 $ randoms (mkStdGen seed)
-
-lineLargeTiles seed = hcat' (with & sep .~ 1) $ map largeTile seeds
-  where seeds = take 4 $ randoms (mkStdGen seed)
-
-setLines seed = vcat' (with & sep .~ 1) $ map lineLargeTiles seeds
-  where seeds = take 4 $ randoms (mkStdGen seed)
+tiles = map tile' $ zip colors angles
+  where 
+    colors = [1..16]
+    angles = [1..16]
 
 diamondTheory :: Diagram B R2
-diamondTheory = setLines 19
-
+diamondTheory = position ( zip (map p2 (zip x y)) tiles) 
+  where
+    x = concat $ common
+    y = concat . transpose $ common
+    common = take 4 $ repeat [1..4]
 
 main = mainWith $ diamondTheory
