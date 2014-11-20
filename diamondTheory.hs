069b6dbf74a5c174fab8245cdffa0b3736218338
Alexis Praga <praga@cerfacs.fr>
2014-11-20 21:16:10 +0100

    Solve the rotation issue. The color is now fixed, only the tiles are rotated randomly.

--- diamondTheory.hs
+++ diamondTheory.hs
@@ -27,12 +27,6 @@ generateColor nb
   | (nb == 0) = black
   | otherwise = white
 
-
-coloursPairs :: [Colour Double] -> [(Colour Double, Colour Double)]
-coloursPairs [] = []
-coloursPairs (x:[]) = []
-coloursPairs (x:y:xs) = (x,y):coloursPairs xs
-
 -- Bug : the enveloppe do not take the linewidth (lw) so we set it to none
 triangleLeft :: Colour Double -> Diagram B R2
 triangleLeft color = triangleRect # rotateBy (1/2) # fc color # lc color # lw none
@@ -41,29 +35,26 @@ triangleRight :: Colour Double -> Diagram B R2
 triangleRight color = triangleRect # fc color #lc color # lw none
 
 -- Color of the right triangle is the inverse of the left triangle
-tile :: Colour Double -> Diagram B R2
-tile color = beside (r2 (1,-1)) (triangleLeft color) (triangleRight c)
-  where 
-    c = if color == black then white else black
+-- We enforce old behaviour for the origin of the tile: we want the point of
+-- tangency, enforced by "align"
+tile :: Diagram B R2
+tile = beside (r2 (1,-1)) (triangleLeft black # align (r2 (1, -1)))
+                                (triangleRight white)
 
---rotateTile :: (Transformable b, V b ~ R2) => b -> Int
 rotateTile :: (Diagram B R2, Int) -> Diagram B R2
 rotateTile x | D.trace ("rotatetile" ++ show (fromIntegral $ snd x)) False = undefined
-rotateTile x = fst x # rotate (x'*pi @@ rad) -- (fromIntegral $ snd x))
+rotateTile x = fst x # rotate (x'*pi/2 @@ rad) 
   where x' = fromIntegral $ snd x
 
 oneLine:: Int -> Diagram B R2
---oneLine seed = cat (r2 (1, 0)) $ map (rotateBy (1/2)) tiles
 oneLine seed = cat (r2 (1, 0)) $ tiles'
   where 
-    tiles' = map rotateTile $ zip tiles angles
-    -- colors = coloursPairs $ map generateColor $ take 8 $ colorNumbers seed
-    tiles = map tile colors
-    colors = map generateColor $ take 4 $ colorNumbers seed
-    angles = [1, 2] -- take 4 $ randomRs (1, 2) (mkStdGen seed)
+    tiles' = map rotateTile $ zip (take 4 $ repeat tile) angles
+    angles = take 4 $ randomRs (1, 4) (mkStdGen seed)
+
 
 diamondTheory :: Diagram B R2
 diamondTheory = cat (r2 (0, 1)) $ map oneLine seeds
-  where seeds = take 4 $ randomRs (0, 1) (mkStdGen 13)
+  where seeds = take 4 $ randoms (mkStdGen 19)
 
 main = mainWith $ diamondTheory
