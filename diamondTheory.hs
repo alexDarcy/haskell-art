bb51dc1a15cf334c508ecba6bd5a0ecbcbb4de99
Alexis Praga <praga@cerfacs.fr>
2014-11-29 10:05:27 +0100

    Smaller code : we use an array of position to place the large tile and pass directly a list of chunks.

--- diamondTheory.hs
+++ diamondTheory.hs
@@ -3,7 +3,7 @@
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import System.Random
-import Data.List
+-- import Data.List
 import Data.List.Split
 --import qualified Debug.Trace as D
 
@@ -15,38 +15,23 @@ triangleRect = polygon ( with
      [ 1        , side      ]
   )
 
-
 colorNumbers :: Int -> [Int]
 colorNumbers seed = randomRs (0, 1) (mkStdGen seed)
 
 randInts :: Int -> [Int]
 randInts seed = randomRs (0, 3) (mkStdGen seed)
 
---listAngles :: Int -> [Angle]
---listAngles seed = map helper $ randomRs (0, 3) (mkStdGen seed) 
-
-listColors :: Int -> [Colour Double]
-listColors seed = map generateColor $ randomRs (0, 1) (mkStdGen seed) 
-
-generateColor :: Int -> Colour Double
-generateColor nb 
-  | (nb == 0) = black
-  | otherwise = white
-
 -- Bug : the enveloppe do not take the linewidth (lw) so we set it to none
-triangleLeft :: Colour Double -> Diagram B R2
-triangleLeft color = triangleRect # rotateBy (1/2) # fc color # lc color # lw none
+triangleLeft = triangleRect # rotateBy (1/2) # fc white # lc white # lw none
 
-triangleRight :: Colour Double -> Diagram B R2
-triangleRight color = triangleRect # fc color #lc color # lw none
+triangleRight = triangleRect # fc black #lc black # lw none
 
 -- Color of the right triangle is the inverse of the left triangle, which is not
 -- modifiable.
 -- We enforce old behaviour for the origin of the tile: we want the point of
 -- tangency, enforced by "align"
-smallTile :: Diagram B R2
-smallTile = beside (r2 (1,-1)) (triangleLeft white # align (r2 (1, -1)))
-                                (triangleRight black)
+smallTile = beside (r2 (1,-1)) (triangleLeft # align (r2 (1, -1)))
+                                (triangleRight)
 
 -- Place a list of 4 object into a matrix
 -- The origin must be placed at the center with align
@@ -66,6 +51,7 @@ mediumTile angles = createMatrix listTiles
 -- Beware reflectX is actually a reflection in respect to the Y axis, so the
 -- naming convention is inverted
 -- Needs a list of 16 angles
+largeTile :: [Int] -> Bool -> Bool -> Diagram B R2
 largeTile angles xSymmetry ySymmetry = createMatrix [a, b, c, d]
   where 
     a = mediumTile $ chunks !! 0
@@ -88,21 +74,15 @@ largeTile' x = largeTile n xSymmetry ySymmetry
     xSymmetry = axis == 1 || axis == 3
     ySymmetry = axis == 2 || axis == 3
 
+centerPos x = (x-0.5)*4 + (x-1)*d
+    where d = 0.5
 
--- Needs a list of 10*16 and 10 elements
-lineTiles :: ([Int], [Int]) -> Diagram B R2
-lineTiles x =  hcat' (with & sep .~ 0.9) $ map largeTile' $ zip n axes
+diamondTheory = position (zip (map p2 pos) (map largeTile' $ zip angles axes))
   where 
-    n = chunksOf 16 $ fst x
-    axes = snd x
-    --n = take 10 $ chunksOf 16 $ randInts 3
-    --axes = take 10 $ randInts 7
+    nb = 3
+    pos = [(centerPos x, centerPos y) | x <- [1..nb], y <- [1..nb]]
+    angles = take (nb*nb) $ chunksOf 16 $ randInts 3
+    axes = take (nb*nb) $ randInts 7
 
-diamondTheory :: Diagram B R2
-diamondTheory =  vcat' (with & sep .~ 0.9) $ map lineTiles $ zip n axes
-  where
-    n = take 10 $ chunksOf (10*16) $ randInts 3
-    axes = take 10 $ chunksOf 10 $ randInts 7
-    --seeds = take 10 $ randInts $ 31
 
 main = mainWith $ diamondTheory
